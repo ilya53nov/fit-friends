525436659cc9af779135117a965824223d3692da
ilya53nov <ilya53nov@yandex.ru>
2023-03-24 00:38:20 +0300

    Добавлены конфиги для сервиса загрузки файлов

--- libs/core/src/lib/config/multer.config.ts
+++ libs/core/src/lib/config/multer.config.ts
@@ -4,13 +4,14 @@ import { randomUUID } from 'crypto';
 import { diskStorage } from 'multer';
 import { join } from 'path';
 import * as mime from 'mime-types';
+import { StaticOption } from './static.config';
 
 export function getMulterConfig(configService: ConfigService): MulterOptions {
-  const uploadFolder = configService.get<string>('static.upload');
+  const uploadFolder = configService.get<string>(StaticOption.Upload.Key);
 
   return {
     storage: diskStorage({
-      destination: join(__dirname, uploadFolder),
+      destination: join(__dirname, uploadFolder!),
       filename: (_req, file, callback) => {
         const extension = mime.extension(file.mimetype) ?? '';
         const filename = randomUUID();
