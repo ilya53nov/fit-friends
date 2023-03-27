21a9686be2c4e73c538056eed89bb773849a2f2d
ilya53nov <ilya53nov@yandex.ru>
2023-03-27 21:31:14 +0300

    Рефакторинг

--- apps/backend/src/app/users/users.controller.ts
+++ apps/backend/src/app/users/users.controller.ts
@@ -1,5 +1,5 @@
 import { AccessTokenGuard, GetUser, Roles, RolesGuard } from '@fit-friends/core';
-import { UpdateCoachUserDto, UpdateSportsmanUserDto, UpdateUserDto } from '@fit-friends/shared-dto';
+import { UpdateCoachUserDto, UpdateSportsmanUserDto } from '@fit-friends/shared-dto';
 import { CoachUserRdo, SportsmanUserRdo } from '@fit-friends/shared-rdo';
 import { ApiRouteEnum, ParametrKey, RoleEnum } from '@fit-friends/shared-types';
 import {
@@ -9,6 +9,7 @@ import {
   Patch,
   Param,
   UseGuards,
+  HttpStatus,
 } from '@nestjs/common';
 import { ApiResponse } from '@nestjs/swagger';
 import { UsersService } from './users.service';
@@ -19,6 +20,7 @@ export class UsersController {
 
   @ApiResponse({
     type: SportsmanUserRdo || CoachUserRdo,
+    status: HttpStatus.OK,
   })
   @Get()
   public async findAll() {
@@ -27,25 +29,55 @@ export class UsersController {
 
   @ApiResponse({
     type: SportsmanUserRdo || CoachUserRdo,
+    status: HttpStatus.OK,
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
   })
   @Get(ParametrKey.Rout)
   public async findOne(@Param(ParametrKey.Id) id: string) {
     return this.usersService.findOne(id);
   }
 
+  @ApiResponse({
+    type: SportsmanUserRdo,
+    status: HttpStatus.OK,
+  })
+  @ApiResponse({
+    status: HttpStatus.BAD_REQUEST,
+  })
+  @ApiResponse({
+    status: HttpStatus.FORBIDDEN,
+  })
   @Roles(RoleEnum.Sportsman)
   @UseGuards(RolesGuard)
   @UseGuards(AccessTokenGuard)
   @Patch(ApiRouteEnum.UpdateSportsman)
-  public async updateSportsman(@GetUser(ParametrKey.Id) userId: string, @Body() updateUserDto: UpdateSportsmanUserDto) {
+  public async updateSportsman(
+    @GetUser(ParametrKey.Id) userId: string,
+    @Body() updateUserDto: UpdateSportsmanUserDto
+  ) {
     return this.usersService.updateSpotsman(userId, updateUserDto);
   }
 
+  @ApiResponse({
+    type: CoachUserRdo,
+    status: HttpStatus.OK,
+  })
+  @ApiResponse({
+    status: HttpStatus.BAD_REQUEST,
+  })
+  @ApiResponse({
+    status: HttpStatus.FORBIDDEN,
+  })
   @Roles(RoleEnum.Coach)
   @UseGuards(RolesGuard)
   @UseGuards(AccessTokenGuard)
   @Patch(ApiRouteEnum.UpdateCoach)
-  public async updateCoach(@GetUser(ParametrKey.Id) userId: string, @Body() updateUserDto: UpdateCoachUserDto) {
+  public async updateCoach(
+    @GetUser(ParametrKey.Id) userId: string,
+    @Body() updateUserDto: UpdateCoachUserDto
+  ) {
     return this.usersService.updateCoach(userId, updateUserDto);
   }
 
