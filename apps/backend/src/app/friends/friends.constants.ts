dc946172d7093bd79b8ff85df589898194dc2270
ilya53nov <ilya53nov@yandex.ru>
2023-04-22 16:29:14 +0300

    Завершение реализации сервиса: Друзья

---
+++ apps/backend/src/app/friends/friends.constants.ts
@@ -0,0 +1,5 @@
+export enum FriendsDescription {
+  YourselfFriendship = 'Вы не можете добавить самого себя в друзья',
+  ExistFriend = 'Данный пользователь уже у вас в друзьях',
+  NotFountFriend = 'Данный пользователь не найден у вас в друзьях',
+}
--- apps/backend/src/app/friends/friends.module.ts
+++ apps/backend/src/app/friends/friends.module.ts
@@ -3,11 +3,9 @@ import { FriendsService } from './friends.service';
 import { FriendsController } from './friends.controller';
 import { FriendsRepository } from './friends.repository';
 import { UserRepository } from '../users/users.repository';
-import { FriendshipRequestRepository } from './repositories/friendship-request.repository';
-import { FriendshipRequestService } from './services/friendship-request.service';
 
 @Module({
   controllers: [FriendsController],
-  providers: [FriendsService, FriendshipRequestService, FriendsRepository, UserRepository, FriendshipRequestRepository],
+  providers: [FriendsService, FriendsRepository, UserRepository],
 })
 export class FriendsModule {}
