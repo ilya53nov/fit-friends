2fe4587b4d38bbeea8c90a093777ce937fa86ac6
ilya53nov <ilya53nov@yandex.ru>
2023-05-02 01:13:58 +0300

    Добавлены юнит тесты для сервиса авторизации

---
+++ apps/backend/src/app/auth/test/auth-test.constants.ts
@@ -0,0 +1,6 @@
+export enum AuthTestDescription {
+  Controller = 'AuthController',
+  Login = 'login should return tokens',
+  RefreshTokens = 'refreshTokens should return new tokens',
+  Register = 'register should return new user'
+}
