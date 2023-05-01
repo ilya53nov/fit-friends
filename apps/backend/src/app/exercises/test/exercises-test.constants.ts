0441926294629cedb83cd2c38fb0280cbe5dd298
ilya53nov <ilya53nov@yandex.ru>
2023-05-02 01:14:31 +0300

    Добавлены юнит тесты для сервиса тренировки WIP

---
+++ apps/backend/src/app/exercises/test/exercises-test.constants.ts
@@ -0,0 +1,8 @@
+export enum ExecisesTestDescription {
+  Controller = 'ExercisesController',
+  Create = 'Create should return new exercise',
+  FindAllByCoachId = 'FindAllByCoachId should return exercises by coachId',
+  FindAll = 'FindAll should return exercises',
+  FindOne = 'FindOne should return exercise by id',
+  Update = 'Update should return updated exercise',
+}
