79d94e67ab57d485286b7218a6e9828008282a84
ilya53nov <ilya53nov@yandex.ru>
2023-05-02 23:24:37 +0300

    Юнит тесты

---
+++ apps/backend/src/app/reviews/test/reviews-test.constants.ts
@@ -0,0 +1,5 @@
+export enum ReviewsTestDescription {
+  Controller = 'ReviewsController',
+  FindByExerciseId = 'FindByExerciseId should return reviews by exerciseId',
+  Create = 'Create should return new review',
+}
